f49e69eac0cd2b7368a2f43c443c3f06213eeb25
Asdertuy <valter.alejandro@hotmail.com>
2021-09-02 03:03:05 -0500

    second commit

--- EcuacionDe2doGrado/ViewController.swift
+++ EcuacionDe2doGrado/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var atxt: UITextField!
+    @IBOutlet var btxt: UITextField!
+   
+    @IBOutlet weak var solbtn: UIButton!
+   
+    @IBOutlet var ctxt: UITextField!
+    @IBOutlet var solb: UITextField!
+    @IBOutlet var sola: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    @IBAction func solbtnAct(_ sender: UIButton) {
+        
+        sola.text = ""
+        solb.text = ""
+        
+        let a: Float = (Float)(atxt.text!)!
+        let b: Float = (Float)(btxt.text!)!
+        let c: Float = (Float)(ctxt.text!)!
+        
+        var Delta: Float
+        var x1: Float
+        var x2: Float
 
-
+        
+        if (a==0){
+            sola.text = "No es una ecuacion de 2do Grado"
+            solb.text = "No es una ecuacion de 2do Grado"
+        }else{
+            Delta = (b*b-(4*a*c))
+            if (Delta==0){
+                x1 = (-b/(2*a))
+                x2 = (-b/(2*a))
+                sola.text = String(x1)
+                solb.text = String(x2)
+            }else{
+                x1 = ((-b + ((sqrt(Delta)))) / 2*a)
+                x2 = ((-b + ((sqrt(Delta)))) / 2*a)
+                sola.text = "/(x1)"
+                solb.text = "/(x2)"
+            }
+        }
+    }
+    
 }
 
